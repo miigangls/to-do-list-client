1d8fb864e8956c6ad7dcc5fc259e1ab967a51334
miigangls <miigangls@gmail.com>
2021-02-10 20:56:02 -0500

    app title

---
+++ src/pages/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const header = () => {
+    return (<div className="row">
+            <header>
+                <h2>Lista de tareas</h2>
+            </header>
+        </div>
+    );
+};
+
+export default header;
