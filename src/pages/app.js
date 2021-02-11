056f9cf8a7b99b8112d6d81bf3e5981e8a4bd661
miigangls <miigangls@gmail.com>
2021-02-11 18:36:00 -0500

    add router

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {useRoutes} from 'hookrouter';
+import Tasks from './layout';
+import Login from './login'
+//import {UserAutContext} from '../providers/userAut'
 
-import Layout from './layout';
+const routes = {
+    '/': () => <Login />,
+    '/tasks': () => <Tasks />
+};
 
-function app(props) {
-    return ( <Layout />
+function App(props) {
+    const routeResult = useRoutes(routes);
+   
+    return (<>
+            {routeResult}
+        </>
     );
 }
 
-export default app;
+export default App;
