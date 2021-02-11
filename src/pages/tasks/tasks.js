d0bde614c50c50c653c82b6bdfd988c41b93a732
miigangls <miigangls@gmail.com>
2021-02-11 13:51:56 -0500

    pages tasks

--- src/pages/tasks/tasks.js
+++ src/pages/tasks/tasks.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import UseTasks from '../../hooks/useTasks'
 
 const task = () => {
+    const { useTasks } = UseTasks('desc')
+    console.log(useTasks)
+
     return (
         <div className="row">
                     
