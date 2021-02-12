95b84a390aec597db16dbd6020d1b492cd1e55d6
miigangls <miigangls@gmail.com>
2021-02-12 17:24:20 -0500

    states components.

---
+++ src/pages/states.js
@@ -0,0 +1,18 @@
+const STATE_INICIAL = {
+    filters:{
+        filter: 'all',
+        sort: 'asc',
+    },
+    login: {
+        email: '',
+        password: ''
+    },
+    register:  {
+        nombre:'',
+        email: '',
+        password: ''
+    }
+    
+}
+
+export default STATE_INICIAL ; 
