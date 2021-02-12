320eead3d8a8d911c7b444810bf52fec723c3775
miigangls <miigangls@gmail.com>
2021-02-12 17:23:14 -0500

    button close session .

--- src/pages/tasks/header.js
+++ src/pages/tasks/header.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import {useCookies}  from 'react-cookie'
+import { closeSession } from '../../firebase/fetch'
+import {COOKIE_NAME, COOKIE_OPTIONS} from '../../firebase/cookies'
 
-const header = () => {
+const Header = () => {
+    const [removeCookie] = useCookies([COOKIE_NAME]);
+
+    async function onClick (e, ) {
+        e.preventDefault();
+        await closeSession() 
+    }
+
+
     return (<div className="row">
-            <header>
+            <header className="input-filter">
+                <div style={{marginRight: 'auto'}} ></div>
                 <h2>Lista de tareas</h2>
+                <div style={{marginLeft: 'auto'}} >
+                    <button onClick={onClick}  className="button-primary">
+                        <i class="fas fa-power-off"></i>
+                    </button>
+                </div>
+                
+                
             </header>
         </div>
     );
 };
 
-export default header;
+export default Header;
