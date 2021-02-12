457c68e023440ceff60e05d181ffad715c4634a6
miigangls <miigangls@gmail.com>
2021-02-11 19:52:38 -0500

    byg fixed, cookie options

--- src/firebase/cookies.js
+++ src/firebase/cookies.js
@@ -1,5 +1,5 @@
-const COOKIE_OPTIONS = "";
 const COOKIE_NAME = 'local';
+let COOKIE_OPTIONS = "";
 
 if (process.env.REACT_APP_NODE_DEV) {
     COOKIE_OPTIONS = { path: '/', expires: new Date('Tue, 19 Jan 2038 03:14:07 UTC') }
