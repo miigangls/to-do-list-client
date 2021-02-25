f927d50ed055f16d52a015139cf6dcb42a2f30f4
miigangls <miigangls@gmail.com>
2021-02-24 21:11:08 -0500

    sort taks

--- src/pages/tasks/tasks.js
+++ src/pages/tasks/tasks.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchActiveTask
     , fetchCompleteTask
     , fetchDeleteTask
@@ -11,8 +11,8 @@ import UseAuthenticated from '../../hooks/useAuthenticated'
 import State from '../states'
 
 
-
 const Task = () => {
+    const [sort, setSort] = useState(true)
     const {userId} =  UseAuthenticated()
     const { useValues , handleChange, handleClear  } = UseComponents(State.tasks);
     let { useTasks, fetchTasks} = UseTasks({userId, order: useValues.sort})
@@ -21,7 +21,7 @@ const Task = () => {
     useEffect(() => {
         fetchTasks()
     }, [useTasks.data.length]);
-
+    
     
     // Create a new task
     async function onClickAdd (e) {
@@ -53,7 +53,29 @@ const Task = () => {
         fetchTasks()
     }
 
-    data = filter(data, useValues.filter) // Filter data based on its status
+
+    async function onClientSort(e) {
+        e.preventDefault()
+        if(sort) {  
+            data = data.sort((a, b) => {
+                if(a.key > b.key) return 1;
+                if(a.key < b.key) return -1;
+                return 0;
+            });
+        }
+        else {
+            data = data.sort((a, b) => {
+                if(a.key > b.key) return -1;
+                if(a.key < b.key) return 1;
+                return 0;
+            });
+        } 
+        setSort(!sort)
+    }
+
+    data = filter(data, useValues.filter) // Filter data based on it's status
+
+    
 
     return (
         <>
@@ -71,26 +93,32 @@ const Task = () => {
                 
         </div>
         <div className="input-filter row">
-        <div style={{marginRight: 'auto'}} ></div>
+            <div style={{marginRight: 'auto'}} ></div>
             <div className="align-items" >
-                <label className="text-label">Fitrar</label>
-                <select  defaultValue={useValues.filter}  name="filter" onChange={handleChange} className="custom-select custom-select-sm btn my-2">
-                    <option value="all">Todas</option>
-                    <option value={false}>Completadas</option>
-                    <option value={true}>Activas</option>
-                </select>
+                <div className="item">
+                    <label className="text-label">Fitrar</label>    
+                    <select  defaultValue={useValues.filter}  name="filter" onChange={handleChange} className="custom-select custom-select-sm btn my-2">
+                        <option value="all">Todas</option>
+                        <option value={false}>Completadas</option>
+                        <option value={true}>Activas</option>
+                    </select>
+                </div>
+                <label className="text-label">Ordenar</label>
+                <div className="item" onClick={onClientSort}> <i className={`fas ${(sort) ? 'fa-sort-amount-down-alt': 'fa-sort-amount-up-alt'}`}></i></div>
             </div>
+            
         </div>
         <div className="row">
             <div className="list-wrapper">
                 <ul>
                     {
-                        data.map(({data, id}, i) => {
-                            return <li key={id} className={(!data.status)? 'completed' : ''}>
+                        data.map((data, i) => {
+                            let {status, id, task, key} = data
+                            return <li key={id} className={(!status)? 'completed' : ''}>
                                 <div className="form-check"> 
                                     <label  className="form-check-label"> 
-                                    <input checked={(!data.status)? true : false}  onChange={(e)=> onClickStatus(e, {...data, id})}  className="checkbox" type="checkbox" /> 
-                                    {`${i + 1}. ${data.task}`} 
+                                    <input checked={(!status)? true : false}  onChange={(e)=> onClickStatus(e, {...data, id})}  className="checkbox" type="checkbox" /> 
+                                    {`${key}. ${task}`} 
                                     <i className="input-helper" ></i>
                                     <a onClick={(e)=> onClickDelete(e, id)} >
                                         <i className="fas fa-trash-alt"></i>    
